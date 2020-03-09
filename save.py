c62c7dc49f6c6562744c9f81160a159447f7fc65
LeraKravts <60606888+LeraKravts@users.noreply.github.com>
2020-03-09 20:23:09 +0300

    Update save.py

--- save.py
+++ save.py
@@ -2,3 +2,4 @@ def save_pokemons(pokemon_list, path_save_dir):
     with open(path_save_dir + '/pokemons.csv', 'a', encoding='utf8') as f:
         for pokemon in pokemon_list:
             f.write(str(pokemon)+'\n')
+            
