19f7ed8aaee85c005055b39122a9a38ab2b4475c
ValeriaKravtsova <lera.gor@inbox.ru>
2020-03-09 18:01:26 +0300

    add parse function

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import requests
+from parsel import Selector
+
+from pokemon import Pokemon
+
+
+def get_pokemons(html: str) -> list:
+    sel = Selector(text=html)
+    pokemon_table = sel.css('table[class="wikitable sortable"]')
+    pokemons = []
+    for row in pokemon_table.css('tr')[1:]:
+        elements = row.css('td')
+        name = elements[2].css('a::text').get().strip()
+        type1 = elements[3].css('a::text').get().strip()
+        type2 = elements[4].css('a::text').get()
+        if type2 is not None:
+            type2 = type2.strip()
+        english = elements[5].css('::text').get().strip()
+        japan = elements[6].css('::text').get().strip()
+        pokemons.append(Pokemon(name, type1, type2, english, japan))
+    return pokemons
 
 
 BASE_URL = 'https://pokemon.fandom.com/ru/wiki/Поколение_I'
-
-
 resp = requests.get(BASE_URL)
+print(get_pokemons(resp.text))
 
 
+resp = requests.get(BASE_URL+'I')
+print(get_pokemons(resp.text))
 
