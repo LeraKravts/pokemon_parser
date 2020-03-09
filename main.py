4387408e9135f6aeb621e81559bb3938adb39546
ValeriaKravtsova <lera.gor@inbox.ru>
2020-03-09 19:45:45 +0300

    add main

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from save import save_pokemons
 BASE_URL = 'https://pokemon.fandom.com/ru/wiki/Поколение_I'
 PATH_SAVE_DIR = '/Users/valeriya/Desktop/'
 
-resp1 = requests.get(BASE_URL)
-resp2 = requests.get(BASE_URL+'I')
 
-if os.path.exists(PATH_SAVE_DIR + 'pokemons.csv'):
-    os.remove(PATH_SAVE_DIR + 'pokemons.csv')
+def main():
+    resp1 = requests.get(BASE_URL)
+    resp2 = requests.get(BASE_URL + 'I')
+
+    if os.path.exists(PATH_SAVE_DIR + 'pokemons.csv'):
+        os.remove(PATH_SAVE_DIR + 'pokemons.csv')
+
+    save_pokemons(get_pokemons(resp1.text), PATH_SAVE_DIR)
+    save_pokemons(get_pokemons(resp2.text), PATH_SAVE_DIR)
 
 
-save_pokemons(get_pokemons(resp1.text), PATH_SAVE_DIR)
-save_pokemons(get_pokemons(resp2.text), PATH_SAVE_DIR)
+if __name__ == '__main__':
+    main()
