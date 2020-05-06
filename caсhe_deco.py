aaa8156145e8ef0270e5af6c25725bae43a5feca
ValeriaKravtsova <lera.gor@inbox.ru>
2020-05-06 16:34:16 +0300

    add url

--- caсhe_deco.py
+++ caсhe_deco.py
@@ -6,10 +6,10 @@ def save_cache(time_in_sec):
         cache_saver = dict()
 
         def wrapper(*args):
-            currect_time = time()
+            current_time = time()
             old_keys = []
             for key in cache_saver:
-                if currect_time - cache_saver[key][1] > time_in_sec:
+                if current_time - cache_saver[key][1] > time_in_sec:
                     old_keys.append(key)
             for key in old_keys:
                 cache_saver.pop(key)
